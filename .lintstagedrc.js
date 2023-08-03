26e90d3e1263edf4b6575e574be371a6c997dc0d
Akinori Ito <voice0726@gmail.com>
2023-08-03 18:05:59 +0900

    fix: remove lint-staged.config.js because of duplication

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '**/*.ts?(x)': () => 'npm run type-check', // Run type-check on changes to TypeScript files.
-  '**/*.{css,less,sass,scss}': (filenames) => `npm run lint:style:fix ${filenames.join(' ')}`,
-  '**/*.(ts|js)?(x)': (filenames) => `npm run lint:es:fix ${filenames.join(' ')}`, // Run ESLint on changes to JavaScript/TypeScript files.
+  '**/*.ts?(x)': () => 'npm run type-check',
+  '**/*.{css,less,sass,scss}': 'npm run lint:style:fix',
+  '**/*.(ts|js)?(x)': ['npm run lint:fix', 'npm run lint:prettier:fix'],
 };
--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': ['npm run lint:es:fix', "bash -c 'npm run type-check'", 'npm run lint:prettier:fix'],
-  '*.{css,scss}': ['npm run lint:style:fix', 'npm run lint:prettier:fix'],
-  '*.{md}': 'npm run lint:prettier:fix',
-};
